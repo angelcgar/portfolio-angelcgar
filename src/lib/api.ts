4f8c26350c75fcb1dfbf9607ccbd7945e2f687c6
Angel Contreras Garcia <146035436+angelcgar@users.noreply.github.com>
2025-05-12 14:38:32 -0600

    refactor:  renombrar funcion que recupera los datos de los proyectos

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,10 +1,10 @@
-import proyectos from '../data/proyectos.json'
+import proyectos from '../data/proyectos.json';
 
-export async function fetchData() {
-  // const response = await fetch('https://api.example.com/data');
-  // if (!response.ok) {
-  //   throw new Error('Network response was not ok');
-  // }
-  const data = proyectos;
-  return data;
+export async function getProjectData() {
+	// const response = await fetch('https://api.example.com/data');
+	// if (!response.ok) {
+	//   throw new Error('Network response was not ok');
+	// }
+	const data = proyectos;
+	return data;
 }
