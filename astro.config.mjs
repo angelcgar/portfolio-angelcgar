c18790ab24fe65f0e966d94409267f52d6da10e6
Angel Contreras Garcia <146035436+angelcgar@users.noreply.github.com>
2025-06-12 12:07:25 -0600

    feat: cambiar la importación del adaptador de Vercel a la versión serverless

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
-import vercel from "@astrojs/vercel";
+import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
   output: "server",
