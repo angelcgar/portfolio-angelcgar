152bbbd424fbcf99a594cfc2f1de3ce92bb0930a
Angel Contreras Garcia <146035436+angelcgar@users.noreply.github.com>
2025-06-14 14:34:55 -0600

    feat: mapear datos de proyectos para estructurar la información de manera más clara

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -5,6 +5,22 @@ export async function getProjectData() {
 	// if (!response.ok) {
 	//   throw new Error('Network response was not ok');
 	// }
-	const data = proyectos;
+
+	const data = proyectos.map((proyecto) => {
+		return {
+			id: proyecto.id,
+			name: proyecto.name,
+			title: proyecto.title,
+			description: proyecto.descriptions,
+			image: proyecto.image,
+			url: proyecto.urlLive,
+			repository: proyecto.urlRepository,
+			technologies: proyecto.technologies,
+			category: proyecto.category,
+			isImportant: proyecto.isImportant,
+		}
+	})
+
+	// const data = proyectos;
 	return data;
 }
