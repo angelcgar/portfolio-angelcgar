54aebe4ef76a2c29f27c6f5aeeac403f1ee21aed
Angel Contreras Garcia <146035436+angelcgar@users.noreply.github.com>
2025-06-24 16:06:28 -0600

    fix: actualizar enlaces de contacto y corregir dirección de correo electrónico en la sección de contacto

--- src/constants.ts
+++ src/constants.ts
@@ -2,6 +2,7 @@ export const profileLinks = {
 	linkedin: 'https://www.linkedin.com/in/angel-contreras-garcia-b45a66290/',
   github: 'https://github.com/angelcgar',
   twitter: 'https://twitter.com/angelcgar',
-  email: 'angel@gmail.com',
+  email: 'ac5802021@gmail.com',
   curriculumVitae: 'https://drive.google.com/file/d/175lZD9TAQzWimmRf9Kca5zqboccSJ28b/view?usp=sharing',
-};
+  name: "angelcgar"
+} as const;
