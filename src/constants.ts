0209ca46efeb751a2d31e0cff44be41a5662f6eb
Angel Contreras Garcia <146035436+angelcgar@users.noreply.github.com>
2025-07-30 09:24:48 -0600

    chore: actualizar el link del cv para la version 1.7

--- src/constants.ts
+++ src/constants.ts
@@ -3,6 +3,6 @@ export const profileLinks = {
   github: 'https://github.com/angelcgar',
   twitter: 'https://twitter.com/angelcgar',
   email: 'ac5802021@gmail.com',
-  curriculumVitae: 'https://drive.google.com/file/d/1U7pyHF9Al1Ejs9crd9GQvYHxMKNOE8st/view?usp=sharing',
+  curriculumVitae: 'https://drive.google.com/file/d/1gTkkog07M_aSR30T4HbzvJxNV8S_w4Id/view?usp=sharing',
   name: "angelcgar"
 } as const;
