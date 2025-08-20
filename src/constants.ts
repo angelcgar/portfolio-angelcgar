b189decf95b81fe0b41e7ea0d994c2dff17bebf6
Angel Contreras Garcia <146035436+angelcgar@users.noreply.github.com>
2025-08-20 14:18:18 -0600

    feat: agregar red social Facebook

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,9 @@
 export const profileLinks = {
-	linkedin: 'https://www.linkedin.com/in/angel-contreras-garcia-b45a66290/',
+  linkedin: 'https://www.linkedin.com/in/angel-contreras-garcia-b45a66290/',
   github: 'https://github.com/angelcgar',
   twitter: 'https://twitter.com/angelcgar',
   email: 'ac5802021@gmail.com',
   curriculumVitae: 'https://drive.google.com/file/d/1gTkkog07M_aSR30T4HbzvJxNV8S_w4Id/view?usp=sharing',
-  name: "angelcgar"
+  name: "angelcgar",
+  facebook: "https://www.facebook.com/angelcgar",
 } as const;
