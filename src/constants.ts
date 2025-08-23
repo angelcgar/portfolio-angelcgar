1198d66d15d291fca42ea9def07cf709ac2d3dfe
Angel Contreras Garcia <146035436+angelcgar@users.noreply.github.com>
2025-08-23 10:53:09 -0600

    feat: agregar mi link de mi instagram

--- src/constants.ts
+++ src/constants.ts
@@ -6,5 +6,6 @@ export const profileLinks = {
   curriculumVitae: 'https://drive.google.com/file/d/1gTkkog07M_aSR30T4HbzvJxNV8S_w4Id/view?usp=sharing',
   name: "angelcgar",
   facebook: "https://www.facebook.com/angelcgar",
+  instagram: "https://www.instagram.com/contrerasc0de/",
   blog: "https://blog-angelcgar.vercel.app/"
 } as const;
